8979e8f004b3bdc886cc18076ae6bf0ee186d809
shivam <shivamkhandelwal18172@gmail.com>
2023-02-24 18:34:29 +0530

    first commit1

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='container ' style={{margin: "82px auto 487px auto"}}>
+        <h2>About the Travel Diary</h2>
+        <p>Travel Diary is created by the team of developers of MNIT JAIPUR. The motive to develop the platform is to help the people to manage their works and list of their targets. It gives the user  power to Add, Read, Execute and Delete their tasks. </p>
+    </div>
+  )
+}
